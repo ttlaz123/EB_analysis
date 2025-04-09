a1e0f0bbfa8177c3d47214f47f1341a9d7ac65a7
ttlaz123 <tongtianliu@gmail.com>
2025-04-09 10:37:55 -0700

    Update full_multicomp.py

--- source/full_multicomp.py
+++ source/full_multicomp.py
@@ -37,7 +37,7 @@ def load_shared_data(input_args):
     SHARED_DATA_DICT['theory_spectra'] = ld.load_cmb_spectra(FILE_PATHS['camb_lensing'],
                                                              FILE_PATHS['dust_models'],
                                                              input_args.theory_comps)
-    SHARED_DATA_DICT['theory_spectra'] = ld.include_ede_spectra(input_args.theory_comps, FILE_PATHS['EDE_spectrum'])
+    SHARED_DATA_DICT['theory_spectra'] = ld.include_ede_spectra(FILE_PATHS['EDE_spectrum'], FILE_PATHS['EDE_spectrum'])
     SHARED_DATA_DICT['bandpasses'] = ld.read_bandpasses(FILE_PATHS['bandpasses'])
     SHARED_DATA_DICT['map_reference_header'] = map_reference_header
     
