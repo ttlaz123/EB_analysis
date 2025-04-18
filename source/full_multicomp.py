1493310e1848abc3445a4f8e02f04bfbaa8e3557
Tom <tongtianliu@gmail.com>
2025-04-17 17:03:46 -0700

    Update full_multicomp.py

--- source/full_multicomp.py
+++ source/full_multicomp.py
@@ -496,7 +496,8 @@ def parallel_simulation(input_args, params_dict):
     """
     sim_indices = range(input_args.sim_start, input_args.sim_start + input_args.sim_num)
     try:
-        with ProcessPoolExecutor() as executor:
+        maxworkers = os.cpu_count()-1
+        with ProcessPoolExecutor(maxworkers=maxworkers) as executor:
             # Submit all tasks to the executor
             future_to_sim = {
                 executor.submit(run_simulation, s,
