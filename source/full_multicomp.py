ee4691c8b5281ffb36057f1ea264e00f91d338dc
Tongtian Liu <liuto@holy8a24301.rc.fas.harvard.edu>
2025-05-19 18:02:31 -0400

    merge

--- source/full_multicomp.py
+++ source/full_multicomp.py
@@ -858,9 +858,9 @@ def main():
                 print("Deletion cancelled. Existing chains will be kept.")
         else:
             print(f"No existing chains to overwrite at: {args.output_path}")
-    if True:
+    if False:
         observed_datas_list = load_all_sims(input_args=args)
-        epd.plot_overlay_sims('BB', observed_datas_list, args.output_path)
+        epd.plot_overlay_sims('EB', observed_datas_list, args.output_path)
     else:
         multicomp_mcmc_driver(args)
 
