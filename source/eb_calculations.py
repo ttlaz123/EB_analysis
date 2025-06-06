9f9a3f6d5e2cc1702b5b63c018770d1a697f9439
Tom <tongtianliu@gmail.com>
2025-06-06 15:02:28 -0700

    Update eb_calculations.py

--- source/eb_calculations.py
+++ source/eb_calculations.py
@@ -341,7 +341,10 @@ def apply_dust(post_travel_dict, bandpasses, params_values):
         syncpow[0] = 0 if np.isinf(syncpow[0]) else syncpow[0]
         dust_dls = dustpow * dust_scale1 * dust_scale2
         sync_dls = syncpow * sync_scale1 * sync_scale2
+        print(np.mean(dls))
+        print(np.mean(dust_dls))
         dls += dust_dls + sync_dls
+        print(np.mean(dls))
         post_travel_dict[used_map] = dls
         
     return post_travel_dict
