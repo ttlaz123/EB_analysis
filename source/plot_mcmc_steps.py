0d3704232bc3f1d6ebc1a55b6f6979ce0e07dd7d
ttlaz123 <tongtianliu@gmail.com>
2025-07-15 13:57:25 -0700

    Update plot_mcmc_steps.py

--- source/plot_mcmc_steps.py
+++ source/plot_mcmc_steps.py
@@ -1,13 +1,14 @@
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt 
+import argparse
 
 import eb_load_data as ld
 import eb_file_paths as fp
 import eb_calculations as ec
 
 
-def plot_param_values(params_values, eb_map, dl_theory, used_map):
+def plot_param_values(params_values, eb_map, dl_theory, used_map, outpath):
     plt.figure()
     for params_value in params_values:
         post_rot_dict = ec.apply_cmb_rotation(eb_map,
@@ -15,9 +16,10 @@ def plot_param_values(params_values, eb_map, dl_theory, used_map):
                                                     dl_theory,
                                                     [used_map])
         plt.plot(post_rot_dict[used_map], label=str(params_value))
+    plt.savefig(outpath)
     plt.show()
 
-def get_plotted_values():
+def get_plotted_values(outpath):
     fede=0.07
     FILE_PATHS = fp.set_file_paths('BK18lf', fede=fede)
     used_map = 'BK18_B95e_BxBK18_B95e_E'
@@ -33,10 +35,13 @@ def get_plotted_values():
     eb_map = {
         used_map: dl_theory['EB_EDE']
     }
-    plot_param_values(params_values, eb_map, dl_theory, used_map)
+    plot_param_values(params_values, eb_map, dl_theory, used_map, outpath)
     
 def main():
-    get_plotted_values()
+    parser =argparse.ArgumentParser()
+    parser.add_argument('-p', '--outpath')
+    args = parser.parse_args()
+    get_plotted_values(args.outpath)
 
 if __name__=='__main__':
     main()
