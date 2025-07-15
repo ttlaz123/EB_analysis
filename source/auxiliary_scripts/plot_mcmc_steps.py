d68204993c12c80e2741134bb246279e09142f4b
ttlaz123 <tongtianliu@gmail.com>
2025-07-15 13:54:19 -0700

    Create plot_mcmc_steps.py

---
+++ source/auxiliary_scripts/plot_mcmc_steps.py
@@ -0,0 +1,42 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt 
+
+import eb_load_data as ld
+import eb_file_paths as fp
+import eb_calculations as ec
+
+
+def plot_param_values(params_values, eb_map, dl_theory, used_map):
+    plt.figure()
+    for params_value in params_values:
+        post_rot_dict = ec.apply_cmb_rotation(eb_map,
+                                                    params_value,
+                                                    dl_theory,
+                                                    [used_map])
+        plt.plot(post_rot_dict[used_map], label=str(params_value))
+    plt.show()
+
+def get_plotted_values():
+    fede=0.07
+    FILE_PATHS = fp.set_file_paths('BK18lf', fede=fede)
+    used_map = 'BK18_B95e_BxBK18_B95e_E'
+    params_values = [
+        {'alpha_CMB': 0},
+        {'alpha_CMB': 0.3},
+        {'alpha_CMB': 0.6},
+        {'alpha_CMB': 0.9},
+    ]
+
+    dl_theory = ld.load_cmb_theory(FILE_PATHS['camb_lensing'])
+    dl_theory = ld.load_ede_spectra(FILE_PATHS['EDE_spectrum'], dl_theory)
+    eb_map = {
+        used_map: dl_theory['EB_EDE']
+    }
+    plot_param_values(params_values, eb_map, dl_theory, used_map)
+    
+def main():
+    get_plotted_values()
+
+if __name__=='__main__':
+    main()
