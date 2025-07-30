befd2a3fb09238d5fec702c103ab860106568d13
ttlaz123 <tongtianliu@gmail.com>
2025-07-30 14:51:57 -0700

    Update plot_mcmc_steps.py

--- source/plotting_scripts/plot_mcmc_steps.py
+++ source/plotting_scripts/plot_mcmc_steps.py
@@ -334,7 +334,8 @@ def plot_dust_eb_spectra_with_bpwf(dl_theory, eb_maps, params_values, bpwf, head
                     label=label)
 
     axs[1].set_title("EB Spectrum After BPWF", fontsize=24)
-    axs[1].set_ylabel(r"$D_b^{EB}$ [$\mu$K$^2$]", fontsize=18)
+    axs[1].set_ylabel(r"$\bar{D}_\ell^{%s}(%s\,\mathrm{GHz})\ [\mu\mathrm{K}^2]$" % 
+                        (specname, freqs[0]), fontsize=18)
     axs[1].set_xlabel(r"Multipole $\ell$", fontsize=18)
     axs[1].grid(True, linestyle='-.', alpha=0.6)
     axs[1].legend(fontsize=18, loc='upper left')
