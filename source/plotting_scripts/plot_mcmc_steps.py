5c7ec2a66220011fc468034869a70a5528017336
ttlaz123 <tongtianliu@gmail.com>
2025-07-30 16:54:49 -0700

    Update plot_mcmc_steps.py

--- source/plotting_scripts/plot_mcmc_steps.py
+++ source/plotting_scripts/plot_mcmc_steps.py
@@ -176,7 +176,7 @@ def plot_theory_diff_steps_ebonly(dl_theory, initial_eb_map, bpwf, header, used_
         ell = ell[:min_len]
         ratio = eb / ee
         axs[0].plot(ell, np.arcsin(2 * ratio) / 4 * 180 / np.pi,  linewidth=2)
-        axs[1].plot(ell, dl_theory['EB_EDE'], label=label, linewidth=2)
+        axs[1].plot(ell, eb, label=label, linewidth=2)
         axs[2].plot(L_BIN_CENTERS, real_file[used_eb_map], linewidth=2, marker='o', linestyle='-')
     # Formatting
     axs[-1].set_xlim([0, 600])
