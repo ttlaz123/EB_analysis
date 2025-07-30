ab7095a9e80797981b5aead7298281359c8ec327
ttlaz123 <tongtianliu@gmail.com>
2025-07-30 15:52:20 -0700

    Update plot_mcmc_steps.py

--- source/plotting_scripts/plot_mcmc_steps.py
+++ source/plotting_scripts/plot_mcmc_steps.py
@@ -546,8 +546,8 @@ def main():
                                   param_combos, args.outpath + '_ldiffmodel.png')
         
         param_combos = [
-        (0.46, 0.5, 370),
-        (0.7, 0, 300)
+        (0.26, 0.5, 370),
+        (0.63, 0, 300)
         ]
         plot_theory_diff_steps_ebonly(dl_theory, initial_eb_map, bpwf, header, used_maps[0], 
                                   param_combos, args.outpath + '_ldiffsim.png', binned_dl_observed_dict)
