671ee235f8610c0072273de2eb6864376d92ada5
Tom <tongtianliu@gmail.com>
2025-08-04 13:19:54 -0700

    Update eb_plot_data.py

--- source/eb_plot_data.py
+++ source/eb_plot_data.py
@@ -775,9 +775,21 @@ def plot_sim_peaks(chains_path, single_sim, sim_nums=None, single_path=None,
         Requires functions `load_summary_csv`, `compute_corner_ranges`, and `make_titles`
         which should return appropriate DataFrames and titles for corner plot.
     """
-    means_df, stds_df, minchisq_df, param_names = load_summary_csv(chains_path)
+    plt.rcParams.update({
+        "text.usetex": True,
+        "font.size": 24,
+        "font.family": "serif",
+        "font.serif": ["Computer Modern"],
+        "axes.labelsize": 24,
+        "xtick.labelsize": 18,
+        "ytick.labelsize": 18,
+        "legend.fontsize": 18,
+    })
 
+    means_df, stds_df, minchisq_df, param_names = load_summary_csv(chains_path)
     selected_params = None
+    
+    
     if(True): 
         selected_params = [
             'alpha_P353e',
@@ -785,6 +797,8 @@ def plot_sim_peaks(chains_path, single_sim, sim_nums=None, single_path=None,
             'alpha_CMB',
             'A_dust_EE'
         ]
+    if selected_params is not None:
+        param_names = [p for p in param_names if p in selected_params]
     latex_labels = {
         'alpha_P353e': r'$\alpha_{\mathrm{P353e}}$',
         'alpha_BK18_220': r'$\alpha_{\mathrm{BK18,\,220}}$',
@@ -793,8 +807,7 @@ def plot_sim_peaks(chains_path, single_sim, sim_nums=None, single_path=None,
     }
     labels = [latex_labels.get(p, p) for p in param_names]
 
-    if selected_params is not None:
-        param_names = [p for p in param_names if p in selected_params]
+    
 
     ranges = compute_corner_ranges(means_df, param_names, percentile_clip)
 
